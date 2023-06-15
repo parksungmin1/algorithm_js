12add65f99f384a2b53733c8de6da6fb3314d7b9
parksungmin1 <86947015+parksungmin1@users.noreply.github.com>
2023-06-15 23:26:10 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.18 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,11 +1,9 @@
 const solution = (array) => {
     let map = new Map();
 
-    for (i of array)
+    for(let i of array)
         map.set(i, (map.get(i)||0) + 1);	
-    console.log(map);
-    ar = [...map].sort((a,b)=>b[1]-a[1]);  
-    console.log(ar);
-    
+
+   let ar = [...map].sort((a,b)=>b[1]-a[1]);  
     return ar.length === 1 || ar[0][1] > ar[1][1] ? ar[0][0] : -1;
 }
