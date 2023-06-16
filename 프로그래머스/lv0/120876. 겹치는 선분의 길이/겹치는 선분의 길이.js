1413bc6b6411689b48d02708dff32036aa96dc33
parksungmin1 <86947015+parksungmin1@users.noreply.github.com>
2023-06-16 18:04:09 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -0,0 +1,13 @@
+function solution(lines) {
+    var sum = 0;
+    for(let i=-100; i<100; i++){
+      let cnt = 0;          
+        if(lines[0][0] <= i  && i<lines[0][1]) {cnt++;}
+        if(lines[1][0] <= i  && i<lines[1][1]) {cnt++;}
+        if(lines[2][0] <= i  && i<lines[2][1]) {cnt++;}
+        
+        if(cnt>1) {sum++;}
+    }
+    return sum;
+    
+}
