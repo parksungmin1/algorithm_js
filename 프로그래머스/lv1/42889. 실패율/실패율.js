5edf684122d2310e87d9a7b7f50879d0f7297e77
parksungmin1 <86947015+parksungmin1@users.noreply.github.com>
2023-06-29 19:37:55 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,4 +1,13 @@
 function solution(N, stages) {
-    var answer = [];
-    return answer;
+    let total = stages.length;
+    let array = [];
+    
+    for(let i=1; i<=N; i++){
+        let fail= stages.filter((s) => s === i).length;
+        array.push([i, fail/total]);
+        total -= fail;
+    }
+    
+    const answer = array.sort((a,b) => b[1] - a[1]);
+    return answer.map(v=> v[0]);
 }
